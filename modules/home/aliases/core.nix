a067ae978540ca66b7da01df22f233741067c468
Bardia Samiee <bardiasamiee@users.noreply.github.com>
2025-10-19 01:39:04 -0500

    add ruff alias

--- modules/home/aliases/core.nix
+++ modules/home/aliases/core.nix
@@ -119,7 +119,7 @@
 
     # --- Dev Tools ----------------------------------------------------------
     pyright = "basedpyright";
-    ruff fix = "ruff check —fix";
+    rfix = "ruff check —fix";
 
     # --- MacOS Specific -----------------------------------------------------
     awake = "caffeinate -dims";                     # Prevent sleep (Ctrl+C to stop)
